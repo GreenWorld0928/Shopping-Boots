e35032695835b8787b966d71c533d35d8cd30297
GreenWorld0928 <green.world928@outlook.com>
2024-04-02 15:35:23 -0500

    Fixed scrolling event interact issue

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useRef } from "react";
-import { Link } from "react-router-dom";
 import {
     makeStyles,
     useTheme,
@@ -7,7 +6,6 @@ import {
     Toolbar,
     Typography,
 } from "@material-ui/core";
-import Product from "../components/Product";
 import {
     Row, Col, ListGroup
 } from "react-bootstrap";
@@ -33,6 +31,7 @@ const useStyles = makeStyles((theme) => ({
         justifyContent: "center",
     },
 }));
+var mScroll = false, totalClicks = 0;
 
 function Home() {
     const classes = useStyles();
@@ -40,22 +39,18 @@ function Home() {
     const [categories, setCategories] = useState([]);
     const [activeCategory, setActiveCategory] = useState(1);
 
-    const innerRef = useRef(null);
-
-    useEffect(() => {
-        getProducts();
-        window.addEventListener('scroll', handleScroll);
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
-
     const scrollToSpecialComponent = (id, index) => {
+        let currentClickNumber = totalClicks
+        mScroll = true;
         const element = document.getElementById(id);
         if (element) {
             element.scrollIntoView({ behavior: 'smooth' });
         }
-        setActiveCategory(index+1);
+        setActiveCategory(index + 1);
+        setTimeout(() => {
+            if (totalClicks === currentClickNumber + 1) mScroll = false
+        }, 1000);
+        totalClicks++;
     };
 
     const getProducts = () => {
@@ -76,22 +71,26 @@ function Home() {
         setCategories(tempCategories);
     }
 
-    const handleScroll = (e) => {
-
+    const handleScroll = () => {
+        if (mScroll) return
         console.log("scrolling...")
         const view = document.getElementById(`viewPort`)
         const categoryRefs = categories.map((_, index) => document.getElementById(`category-${index + 1}`));
-        if(!categoryRefs||categoryRefs.length==0) return
+        if (!categoryRefs || categoryRefs.length == 0) return
         console.log("categoryRefs...", categoryRefs[0].offsetTop)
-        console.log("scrollPosition...",view.offsetTop+view.scrollTop)
+        console.log("scrollPosition...", view.offsetTop + view.scrollTop)
         for (let i = categoryRefs.length - 1; i >= 0; i--) {
             const categoryRef = categoryRefs[i];
-            if (categoryRef.offsetTop <= view.offsetTop+view.scrollTop) {
+            if (categoryRef.offsetTop <= view.offsetTop + view.scrollTop) {
                 setActiveCategory(i + 1);
                 break;
             }
         }
     };
+
+    useEffect(() => {
+        getProducts();
+    }, [])
 
     return (
         <React.Fragment>
@@ -126,7 +125,7 @@ function Home() {
                     </ListGroup>
                 </Col>
                 <Col lg="10" className="">
-                    <div id="viewPort" onScroll={(e)=>handleScroll(e)} className="border" style={{ position: 'relative', overflowX: 'hidden', overflowY: 'scroll', height: '700px' }}>
+                    <div id="viewPort" onScroll={handleScroll} className="border" style={{ position: 'relative', overflowX: 'hidden', overflowY: 'scroll', height: '700px' }}>
                         <h3 className="p-4">FASTLANE 2.0 Product</h3>
                         <p className="ps-4">Fast Lane orders are produced in just 1-Week, almost 4 times faster than standard MTO orders. However, Fast Lane customizing options are slightly limited, especially regarding materials.</p>
                         {categories.map((type, index) => (
